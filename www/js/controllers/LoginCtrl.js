c643690f6aaa203653391517ceeca67f067859fa
Raul Pino <pinox101@gmail.com>
2016-08-31 13:28:18 -0300

    login page logic fixed

--- www/js/controllers/LoginCtrl.js
+++ www/js/controllers/LoginCtrl.js
@@ -2,30 +2,35 @@ angular.module('LateralApp')
 
 .controller('LoginCtrl', function($scope, $rootScope, $cordovaInAppBrowser, $state, ServerAPI) {
 
-  // TODO include logic for already logged in user
+  // TODO include logic for already logged in user, and error cases
 
   $scope.loginFacebook = function() {
     $cordovaInAppBrowser.open(ServerAPI.FACEBOOK_AUTH_URL, '_blank', {location: 'no'})
     .catch(function(event) {
-      // TODO show error and return to login
       $state.go('login');
     });
 
     $rootScope.$on('$cordovaInAppBrowser:loadstop', function(e, event){
-      // method found
-      // https://dbwriteups.wordpress.com/2016/01/24/sharing-data-between-hybrid-app-and-inapp-browser/
-      // http://ngcordova.com/docs/plugins/inAppBrowser/
-      setTimeout(getTokenFromWindow, 1000);
+      getTokenFromWindow()
+      .then(setTokenAndLogin);
     });
   };
 
     function getTokenFromWindow(){
-      $cordovaInAppBrowser.executeScript({code: "localStorage.getItem('feathers-jwt')"})
-      .then(function(values) {
-        var token = R.head(values);
-        setTokenAndLogin(token);
+      return new Promise(function(resolve, reject) {
+        // method found
+        // https://dbwriteups.wordpress.com/2016/01/24/sharing-data-between-hybrid-app-and-inapp-browser/
+        // http://ngcordova.com/docs/plugins/inAppBrowser/
+        setTimeout(function() {
+
+          $cordovaInAppBrowser.executeScript({code: "localStorage.getItem('feathers-jwt')"})
+          .then(function(values) {
+            var token = R.head(values);
+            resolve(token);
+          });
+
+        }, 1000);
       });
-      // TODO show error and return to login
     }
 
     function setTokenAndLogin(token) {
@@ -36,11 +41,12 @@ angular.module('LateralApp')
       // set especial localStorage token for Feathers client http://docs.feathersjs.com/authentication/readme.html#options
       window.localStorage.setItem('feathers-jwt', token);
 
-      ServerAPI.app.authenticate().then(function(result){
-        // close window
-        $cordovaInAppBrowser.close();
+      ServerAPI.app.authenticate()
+      .then(function(result){
         // redirect to dashboard
         $state.go('tab.status');
+        // close window
+        setTimeout(function() { $cordovaInAppBrowser.close(); }, 1000);
       }).catch(function(error){
         // redirect to login
         window.localStorage.removeItem('feathers-jwt');

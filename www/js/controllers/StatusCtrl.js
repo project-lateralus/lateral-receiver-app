6c3eba2c6f29acf709f4c5c5c07ed0d3e9e346e3
Raul Pino <pinox101@gmail.com>
2016-09-01 09:46:04 -0300

    server status implemented in its section

--- www/js/controllers/StatusCtrl.js
+++ www/js/controllers/StatusCtrl.js
@@ -1,3 +1,18 @@
 angular.module('LateralApp')
 
-.controller('StatusCtrl', function($scope) {});
+.controller('StatusCtrl', function($scope, config, SocketServerAPI) {
+
+  $scope.server = {
+    url: config.server.url,
+    isOnline: false
+  };
+
+  SocketServerAPI.socket.on('connect', updateServerData);
+  SocketServerAPI.socket.on('disconnect', updateServerData);
+
+  function updateServerData() {
+    $scope.server.isOnline = SocketServerAPI.socket.connected;
+    $scope.$apply();
+  }
+
+});
